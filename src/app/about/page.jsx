f4325b48a4ab409fb74d7d7706a19bd81c3d7886
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-03-27 21:26:18 +0330

    This project was created for my practice and is not a real or practical project and has no credit.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Hi Next | this about page</h1>
+      </div>
+    );
+  }
+  
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hi Next | this is Home Page</h1>
+    </div>
+  );
+}
